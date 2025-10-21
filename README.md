9f7ad3d917a6efc4ed741761e43804e9d2c422df
chenkai <1436632438@qq.com>
2025-10-21 15:47:48 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# lp_jsonPaser_canvas
+# lp_json_paser_canvas
+
+### json 文件解析器
+1.支持json文件结构的解析
+2.支持json加转义
+3.支持json去转译
+4.支持.lp2文件的json结构解析
---
+++ android/app/src/main/kotlin/com/example/lp_json_paser/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.lp_json_paser
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
